b0db6f4542172c225ad0e5afdcb92315391182a4
Romain Semler <romain.semler@gmail.com>
2016-04-05 01:46:49 +0200

    Update connect-bd.php

--- php/controller/connect-bd.php
+++ php/controller/connect-bd.php
@@ -2,7 +2,6 @@
   try
   {
     $bdd = new PDO('mysql:host=localhost;dbname=picstore;charset=utf8', 'root', 'root');
-    //$bdd = new PDO('mysql:host=mysql.hostinger.fr;dbname=u765007937_base;charset=utf8', 'u765007937_id', 'ponsestunesalope');
   }
   catch (Exception $e)
   {
