e3113cc6b5bc5bb3f772f5194b919ea061ce309b
Semler Romain <romain.semler@gmail.com>
2016-04-16 19:29:10 +0200

    Revert "enfin la modif du compte"
    This reverts commit 6e413c37dea0c3cd875ce6fc5387af2aacf6912c.

--- user.php
+++ user.php
@@ -1,4 +1,4 @@
-<?php	session_start();?><!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 	<head>
 	    <title>Picstore</title>
@@ -42,7 +42,7 @@
 
  			<!-- MODIFICATION INFO -->
 	        <div id="bouton-modif" class="centrer">
-	        	<a href="#" class="btn btn-primary" id="boutonText"></a>
+	        	<a href="#" class="btn btn-primary" onclick="displayPanelModif()">Modifier</a>
 	        </div>
 
 	        <div id="modif-container" class="container">
