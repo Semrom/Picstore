5d7b29d38f57a265c296f8f39132cf6568e2ce35
Semler Romain <romain.semler@gmail.com>
2016-04-18 00:32:03 +0200

    Changement de mdp et avatar fonctionnel !

--- js/modif_compte.js
+++ js/modif_compte.js
@@ -1,26 +1,11 @@
 $(document).ready(function () {
-	$("#modif-container").hide();
-});
-
-var isDisplay = false;
-
-function displayPanelModif() {
-	//$("#modif-container").fadeIn();
-    //$("body").fadeTo(400, "0.5");
-    //$("#modif-container").slideDown(500);
-
-    if (isDisplay) {
-        $("#modif-container").slideUp();
-        isDisplay = false;
-    }
-    else {
-        $("#modif-container").slideDown(500);
-        isDisplay = true;
-    }
-}
-
-function removePanelModif() {
-    //document.getElementById('modif-container').removeChild(input.parentNode);
-    $("#modif-container").slideUp();
-    $('body').fadeTo(400, "1");
-}
+		/* RÉCUPÈRE LES PARAMÈTRES DE L'URL */		function getUrlParameter(sParam)	{	    var sPageURL = window.location.search.substring(1);	    var sURLVariables = sPageURL.split('&');	    for (var i = 0; i < sURLVariables.length; i++)	    {	        var sParameterName = sURLVariables[i].split('=');	        if (sParameterName[0] == sParam)	        {	            return sParameterName[1];	        }	    }	    	    return "FAIL";	}		$("#modif-container").hide();		$('#boutonText').text("Modifer le profil");		var isDisplay = false;			var erreur = getUrlParameter('err');	var success = getUrlParameter('succ');		if (erreur != "FAIL") {				$("#modif-container").slideDown(500);		isDisplay = true;		$('#boutonText').text("Ne plus modifer le profil");	}		if (success != "FAIL") {				$('#ok-zone').fadeIn(500);	}		
+	$('#boutonText').click(function() {
+	
+	    if (isDisplay) {
+	        $("#modif-container").slideUp();
+	        isDisplay = false;	        $('#boutonText').text("Modifer le profil");	    } else {
+	        $("#modif-container").slideDown(500);
+	        isDisplay = true;	        $('#boutonText').text("Ne plus modifer le profil");
+	    }
+	});		});
--- php/model/User.php
+++ php/model/User.php
@@ -1,4 +1,4 @@
- <?php  
+<?php  
 /**
 * @author Romain Semler
 *
@@ -9,7 +9,7 @@
 * @brief Classe en charge des informations de l'utilisateur.
 */
 
-class User {
+	class User {
 
 		public $bdd;
 
@@ -25,26 +25,15 @@ class User {
 			$req_info->closeCursor();
 
 			return $donnees;
-		}				/* Récupère le pseudo d'un utilisateur par son id */		public function getPseudo($id) {			$req_pseudo = $this->bdd->prepare('SELECT pseudo_user FROM utilisateur WHERE id_user=:id');			$req_pseudo->execute(array('id' => $id));			$donnees = $req_pseudo->fetch();			$req_pseudo->closeCursor();			return $donnees;		}
+		}				/* Récupère le pseudo d'un utilisateur par son id */		public function getPseudo($id) {			$req_pseudo = $this->bdd->prepare('SELECT pseudo_user FROM utilisateur WHERE id_user=:id');			$req_pseudo->execute(array('id' => $id));			$donnees = $req_pseudo->fetch();			$req_pseudo->closeCursor();			return $donnees;		}				/* Récupère l'avatar d'un utilisateur par son id */		public function getAvatar($id) {			$req_avatar = $this->bdd->prepare('SELECT avatar_user FROM utilisateur WHERE id_user=:id');			$req_avatar->execute(array('id' => $id));			$donnees = $req_avatar->fetch();			$req_avatar->closeCursor();			return $donnees;		}
+		/* Vérifie si le mot de passe actuel est correct */		public function verifierMdp($id_user, $mdp) {			$req_verif_mdp = $this->bdd->prepare('SELECT * FROM utilisateur WHERE id_user = :id AND mdp_user = :mdp');			$req_verif_mdp->execute(array('id' => $id_user, 'mdp' => $mdp));			$rep = $req_verif_mdp->fetch();			$req_verif_mdp->closeCursor();						if (empty($rep)) {				return false;			} else {				return true;			}		}
+    	/* Modifie le mot de passe d'un utilisateur */
+		public function modifierMdp($id_user, $mdp) {
 
-    	/* Change le mot de passe d'un utilisateur */
-		public function changePassword($email, $actual_password, $new_password, $new_password_confirm) {
-			$req_mdp = $this->bdd->prepare('SELECT mdp_user FROM utilisateur WHERE email_user=:email');
-			$req_mdp->execute(array('email' => $email));
-			$donnees = $req_mdp->fetch();
-			$req_mdp->closeCursor();
+			$req_new_mdp = $this->bdd->prepare('UPDATE utilisateur SET mdp_user = :password WHERE id_user = :id');
+			$req_new_mdp->execute(array('password' => $mdp, 'id' => $id_user));
+			$req_new_mdp->closeCursor();
 
-			if ($actual_password == $password['password'])
-			{
-				$req_new_mdp = $this->bdd->prepare('UPDATE utilisateur SET mdp_user=:password WHERE email_user=:email');
-				$req_new_mdp->execute(array('password' => $new_password, 'email' => $email));
-				$req_new_mdp->closeCursor();
-
-				return "OK";
-			}
-			else
-			{
-				return "error_actual_mdp";
-			}
-		}				/* Récupère toutes les informations de tous les utilisateurs */		public function getAllUsers() {			$req_getUsers = $this->bdd->prepare('SELECT id_user, pseudo_user, email_user, DATE_FORMAT(date_inscription_user, \'%d / %m / %Y\') AS date_inscription_user, actif FROM utilisateur ORDER BY id_user DESC');			$req_getUsers->execute();			$donnees = $req_getUsers->fetchAll();      		$req_getUsers->closeCursor();			return $donnees;		}				/* Supprime un utilisateur de la base par son id */		public function deleteUser($id_user) {			$req_deleteUser = $this->bdd->prepare('DELETE FROM utilisateur WHERE id_user = :id');			$req_deleteUser->execute(array('id' => $id_user));      		$req_deleteUser->closeCursor();			return true;		}
+			return true;
+		}				/* Récupère toutes les informations de tous les utilisateurs */		public function getAllUsers() {			$req_getUsers = $this->bdd->prepare('SELECT id_user, pseudo_user, email_user, DATE_FORMAT(date_inscription_user, \'%d / %m / %Y\') AS date_inscription_user, actif FROM utilisateur ORDER BY id_user DESC');			$req_getUsers->execute();			$donnees = $req_getUsers->fetchAll();      		$req_getUsers->closeCursor();			return $donnees;		}				/* Supprime un utilisateur de la base par son id */		public function deleteUser($id_user) {			$req_deleteUser = $this->bdd->prepare('DELETE FROM utilisateur WHERE id_user = :id');			$req_deleteUser->execute(array('id' => $id_user));      		$req_deleteUser->closeCursor();			return true;		}				public function ajouterAvatar($lien, $id_user) {					$req_select = $this->bdd->prepare('SELECT avatar_user FROM utilisateur WHERE id_user = :id');			$req_select->execute(array('id' => $id_user));			$ancien = $req_select->fetch();			$req_select->closeCursor();						if ($ancien['avatar_user'] != "defaut.png") {								unlink('img/avatar/' . $ancien['avatar_user']);			}				  	$req_avatar = $this->bdd->prepare('UPDATE utilisateur SET avatar_user = :lien WHERE id_user = :id');			$req_avatar->execute(array('lien' => $lien, 'id' => $id_user));			$req_avatar->closeCursor();						return true;		}
 	}
