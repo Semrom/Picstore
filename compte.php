be8779c98930cf830e6c26fae735342d54e2a23c
khorgoht <khorgoht@gmail.com>
2016-04-24 22:06:06 +0200

    fonctionnalités de base de la galerie sur la page mon compte a tester

--- compte.php
+++ compte.php
@@ -1,59 +1,77 @@
 <?php
-  session_start();    include_once("php/controller/modif_compte.php");
+session_start();    include_once("php/controller/modif_compte.php");
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-	    <title>Picstore | Mon compte</title>
-	    <meta charset="UTF-8" />
-	    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <head>
+        <title>Picstore | Mon compte</title>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
       <link rel="stylesheet" href="css/style.css" />
-	    <link rel="stylesheet" href="css/bootstrap.css" />
-	    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/bootstrap.css" />
+        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 
-	</head>
+    </head>
 
-	<body>
+    <body>
 
-	    <?php if (isset($_SESSION['user'])) { ?>	    	<!-- MENU -->	    	<?php include_once('php/view/modules/menu.php'); ?>	    <?php } ?>
+        <?php if (isset($_SESSION['user'])) { ?>	    	<!-- MENU -->	    	<?php include_once('php/view/modules/menu.php'); ?>	    <?php } ?>
 
-	    <!-- CONTENEUR D'EN-TETE -->
-	    <div class="container">	    <?php if (isset($_SESSION['user'])) { ?>
-	    	<div class="page-header" id="banner">
-	    	    <div class="row">
-	    	        <div class="col-lg-12 col-md-7 col-sm-6" id="sous-titre-bloc">
-	    	        </div>
-	    	    </div>
-	    	</div>
-	    </div>
+        <!-- CONTENEUR D'EN-TETE -->
+        <div class="container">	    <?php if (isset($_SESSION['user'])) { ?>
+            <div class="page-header" id="banner">
+                <div class="row">
+                    <div class="col-lg-12 col-md-7 col-sm-6" id="sous-titre-bloc">
+                    </div>
+                </div>
+            </div>
+        </div>
 
-	    <div class="container">		    		    <div class="alert alert-dismissible alert-success centrer" id="ok-zone">		      <button type="button" class="close" data-dismiss="alert">×</button>		      <strong>C'est fait !</strong> <br />Vos informations ont bien été mises à jour.		    </div>		    
-	        <!-- AVATAR -->
-	        <div id="avatar-container" class="container">
-	            <div class="ratio img-responsive img-circle" <?php $user = new User($bdd); $img = $user->getAvatar($_SESSION['user']['id_user']); echo 'style="background-image:url(img/avatar/' . $img['avatar_user'] . ');"'; ?>></div>
-	        </div>
-	        <div id="pseudo_user" class="centrer">
-	            <h1><?php echo $_SESSION['user']['pseudo_user']; ?></h1>
-	        </div>
+        <div class="container">		    		    <div class="alert alert-dismissible alert-success centrer" id="ok-zone">		      <button type="button" class="close" data-dismiss="alert">×</button>		      <strong>C'est fait !</strong> <br />Vos informations ont bien été mises à jour.		    </div>		    
+            <!-- AVATAR -->
+            <div id="avatar-container" class="container">
+                <div class="ratio img-responsive img-circle" <?php $user = new User($bdd); $img = $user->getAvatar($_SESSION['user']['id_user']); echo 'style="background-image:url(img/avatar/' . $img['avatar_user'] . ');"'; ?>></div>
+            </div>
+            <div id="pseudo_user" class="centrer">
+                <h1 id="pseudo_user" data-id="<?php echo $_SESSION['user']['id_user']; ?>"><?php echo $_SESSION['user']['pseudo_user']; ?></h1>
+            </div>
 
-	        <div id="nb_like_user" class="centrer">
-	            <h2>13423 J'aime</h2>
-	        </div>
+            <div id="nb_like_user" class="centrer">
+            <h2>13423 J'aime</h2>
+            </div>
 
           <div class="centrer">
-	            <h2>Inscrit le : <?php echo $_SESSION['user']['date_inscription_user']; ?></h2>
-	        </div>	        	        <!-- MODIFICATION INFO -->	        <div id="bouton-modif" class="centrer">	        	<a href="#" class="btn btn-primary" id="boutonText"></a>	        </div>	        <div id="modif-container" class="container">	        	<div class="alert alert-dismissible alert-danger centrer" <?php echo $style; ?> id="error-zone">	        	  <p id="error-text"><?php echo $erreur; ?></p>	        	</div>				<form class="form-horizontal" id="form-password" method="POST" action="compte.php?err=true" enctype="multipart/form-data">				  	<fieldset>				    	<legend>Changer mon mot de passe :</legend>				    	<div class="form-group">				      		<label class="col-lg-2 control-label" for="inputPasswordActual">Mot de passe actuel :</label>				      		<div class="col-lg-10">				        		<input type="password" class="form-control" id="inputPasswordActual" name="actual_pass" <?php if(isset($_POST['actual_pass'])) { echo 'value="' . $_POST['actual_pass'] . '"'; }?> placeholder="Mot de passe actuel">				      		</div>				    	</div>				    	<div class="form-group">				      		<label class="col-lg-2 control-label" for="inputPasswordNew">Nouveau mot de passe :</label>				      		<div class="col-lg-10">				        		<input type="password" class="form-control" id="inputPasswordNew" name="new_pass" <?php if(isset($_POST['new_pass'])) { echo 'value="' . $_POST['new_pass'] . '"'; }?> placeholder="Nouveau mot de passe">				      		</div>				    	</div>				    	<div class="form-group">				    			<label class="col-lg-2 control-label" for="inputPasswordConf">Confirmation :</label>				    			<div class="col-lg-10">				    			<input type="password" class="form-control" id="inputPasswordConf" name="new_pass_confirm" <?php if(isset($_POST['new_pass_confirm'])) { echo 'value="' . $_POST['new_pass_confirm'] . '"'; }?> placeholder="Confirmez le nouveau mot de passe">				    			</div>				    	</div>				    	<br />				    	<legend>Changer mon avatar : </legend>				    	<p>Formats acceptés :   | <strong>.jpg</strong>  |  <strong>.jpeg</strong>  |  <strong>.png</strong> |</p>				    	<div class="form-group">				      		<label class="col-lg-2 control-label" for="inputFile">Avatar (2 Mo max.) :</label>				      		<div class="col-lg-10">				        		<input type="file" class="form-control" id="inputFile" name="avatar" >				      		</div>				    	</div>				    	<input type="hidden" name="id" <?php echo 'value="' . $_SESSION['user']['id_user'] . '"'; ?> />				    	<div id="btn-valid-modif" class="centrer">				    		<button type="submit" class="btn btn-primary">Valider</button>							<button type="reset" class="btn btn-default">Annuler</button>						</div>					</fieldset>				  				</form>	        </div>
+                <h2>Inscrit le : <?php echo $_SESSION['user']['date_inscription_user']; ?></h2>
+            </div>	        	        <!-- MODIFICATION INFO -->	        <div id="bouton-modif" class="centrer">	        	<a href="#" class="btn btn-primary" id="boutonText"></a>	        </div>	        <div id="modif-container" class="container">	        	<div class="alert alert-dismissible alert-danger centrer" <?php echo $style; ?> id="error-zone">	        	  <p id="error-text"><?php echo $erreur; ?></p>	        	</div>				<form class="form-horizontal" id="form-password" method="POST" action="compte.php?err=true" enctype="multipart/form-data">				  	<fieldset>				    	<legend>Changer mon mot de passe :</legend>				    	<div class="form-group">				      		<label class="col-lg-2 control-label" for="inputPasswordActual">Mot de passe actuel :</label>				      		<div class="col-lg-10">				        		<input type="password" class="form-control" id="inputPasswordActual" name="actual_pass" <?php if(isset($_POST['actual_pass'])) { echo 'value="' . $_POST['actual_pass'] . '"'; }?> placeholder="Mot de passe actuel">				      		</div>				    	</div>				    	<div class="form-group">				      		<label class="col-lg-2 control-label" for="inputPasswordNew">Nouveau mot de passe :</label>				      		<div class="col-lg-10">				        		<input type="password" class="form-control" id="inputPasswordNew" name="new_pass" <?php if(isset($_POST['new_pass'])) { echo 'value="' . $_POST['new_pass'] . '"'; }?> placeholder="Nouveau mot de passe">				      		</div>				    	</div>				    	<div class="form-group">				    			<label class="col-lg-2 control-label" for="inputPasswordConf">Confirmation :</label>				    			<div class="col-lg-10">				    			<input type="password" class="form-control" id="inputPasswordConf" name="new_pass_confirm" <?php if(isset($_POST['new_pass_confirm'])) { echo 'value="' . $_POST['new_pass_confirm'] . '"'; }?> placeholder="Confirmez le nouveau mot de passe">				    			</div>				    	</div>				    	<br />				    	<legend>Changer mon avatar : </legend>				    	<p>Formats acceptés :   | <strong>.jpg</strong>  |  <strong>.jpeg</strong>  |  <strong>.png</strong> |</p>				    	<div class="form-group">				      		<label class="col-lg-2 control-label" for="inputFile">Avatar (2 Mo max.) :</label>				      		<div class="col-lg-10">				        		<input type="file" class="form-control" id="inputFile" name="avatar" >				      		</div>				    	</div>				    	<input type="hidden" name="id" <?php echo 'value="' . $_SESSION['user']['id_user'] . '"'; ?> />				    	<div id="btn-valid-modif" class="centrer">				    		<button type="submit" class="btn btn-primary">Valider</button>							<button type="reset" class="btn btn-default">Annuler</button>						</div>					</fieldset>				  				</form>	        </div>
       </div>
       <?php } else { ?>      	<div class="page-header" id="banner">      		    <div class="row">      		        <div class="col-lg-12 col-md-7 col-sm-6" id="sous-titre-bloc">      		        	<p id="sous-titre-text"></p>      		        </div>      		    </div>      		</div>      	</div>      	          <div class="centrer">                <h1>Erreur : connectez-vous !</h1><br />              <a href="./" class="btn btn-info">Retour</a>          </div>
       <?php } ?>
+	        <!-- GRILLE DE GALLERIES -->
+            <div id="album" class="container album">
+                <div id="control-bar-album" class="container-fluid">
+                    <h4 class="col-xs-5 col-xs-offset-4 col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 centrer"><?php echo 'Galeries de ' . $infos['pseudo_user']; ?></h4>
+                </div>
 
-	    <!-- PIED DE PAGE -->
-	    <?php include_once('php/view/modules/footer.php'); ?>
+                <div id="album-content" class="container album-content">
+                    <div id="loading" class="loading"></div>
+                </div>
+	                <!-- Bouton +
+	                <div class="col-xs-3">
+	                    <div class="dummy"></div>
+	                    <div class="in add" role="button">
+	                        <span class="glyphicon glyphicon-plus"></span>
+	                    </div>
+	                </div> -->
+	        </div>
 
-	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	    <script type="text/javascript" src="js/bootstrap.min.js"></script>	    <script type="text/javascript" src="js/modif_compte.js"></script>
-	</body>
+        <!-- PIED DE PAGE -->
+        <?php include_once('php/view/modules/footer.php'); ?>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>	    <script type="text/javascript" src="js/modif_compte.js"></script>
+        <script type="text/javascript" src="js/modif_compte.js"></script>
+    </body>
 
 </html>
---
+++ js/compte.js
@@ -0,0 +1,263 @@
+var wall, wallConfig = {
+    "width": 150,
+    "height": 150
+};
+
+var galeries = { /* variable qui stock toutes les galeries disponibles a afficher, a charger une seule fois au debut */
+        "items": [{
+            "title": "Uploads",
+            "thumbnail": "test.jpg",
+            "id_galerie": 0
+        }, {
+            "title": "Favoris",
+            "thumbnail": "test.jpg",
+            "id_galerie": 1
+        }, {
+            "title": "Images turfesques",
+            "thumbnail": "test.jpg",
+            "id_galerie": 2
+        }, {
+            "title": "Galerie lourde",
+            "thumbnail": "test.jpg",
+            "id_galerie": 3
+        }],
+        "size": 4
+    },
+    contentGalerie = { /*variable qui stock le contenue d'une galerie , a charger avec ajax avant de l'utiliser */
+        "title": "Something",
+        "items": [{
+            "title": "Lourdeur",
+            "link": "test.jpg",
+            "thumbnail": "test.jpg"
+        }, {
+            "title": "Epic",
+            "link": "test.jpg",
+            "thumbnail": "test.jpg"
+        }, {
+            "title": "MRW ta geule",
+            "link": "test.jpg",
+            "thumbnail": "test.jpg"
+        }, {
+            "title": "Red",
+            "link": "FatGuyShootingRed.gif",
+            "thumbnail": "FatGuyShootingRed.gif"
+        }, {
+            "title": "Blue",
+            "link": "FatGuyShootingBlue.gif",
+            "thumbnail": "FatGuyShootingBlue.gif"
+        }, {
+            "title": "who",
+            "link": "test.jpg",
+            "thumbnail": "test.jpg"
+        }, {
+            "title": "piouu",
+            "link": "test2.jpg",
+            "thumbnail": "test2.jpg"
+        }, {
+            "title": ",piouu",
+            "link": "test.jpg",
+            "thumbnail": "test.jpg"
+        }, {
+            "title": "^_^",
+            "link": "test.jpg",
+            "thumbnail": "test.jpg"
+        }],
+        "size": 9
+    };
+
+$(document).ready(function() {
+    wall = new Freewall("#album-content");
+    prepareLoadingGif();
+    $.ajax({
+        url: 'php/controller/get_galleries_and_images.php',
+        type: 'POST',
+        data: 'id_user=' + $("#pseudo_user").data('id') + '&op=Galeries',
+        dataType: 'json',
+        success: function(data) {
+            galeries = data;
+        },
+        complete: function(result, statut) {
+            loadWall(galeries);
+            $(".cell").one("click", function() {
+                ajaxEnterGalerie($(this));
+            });
+        },
+        error: function(result, statut, erreur) {
+            alert("Echec du chargement des galeries, erreur: " + erreur);
+        }
+    });
+});
+
+/*
+ * addControlBar()
+ *
+ * La fonction ajoute une barre de controle pour sortir de la galerie et revenir vers la liste de
+ * galeries
+ *
+ */
+function addControlBar() {
+    var htmlInsideAlbum =
+        "<button id='returnBtn' class='btn btn-default col-xs-3 col-sm-2 col-md-1' type='button'>Retour</button>";
+    $("#control-bar-album").prepend(htmlInsideAlbum);
+    $("#returnBtn").one("click", function() {
+        leaveGalerie(galeries);
+    });
+}
+
+/*
+ * leaveGalerie(contents, contentsClick)
+ *
+ * Cette fonction va quitter l'affichage d'une galerie et afficher les items du parametres contents. 
+ *
+ */
+function leaveGalerie(contents) {
+    $("#control-bar-album").fadeOut(function() {
+        $("#returnBtn").remove();
+        var albumsListTitle = "Galeries de " + $("h1").text();
+        $("h4").text(albumsListTitle);
+        $("h4").removeClass();
+        $("h4").addClass(
+            "col-xs-5 col-xs-offset-4 col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 centrer"
+        );
+    });
+    $("#album-content").fadeOut(function() {
+
+        $("#album-content a").remove();
+        wall.refresh();
+        wall.fitWidth();
+        loadWall(contents);
+        $(".cell").one("click", function() {
+            /* charger avec ajax le contenue de la galerie dans contentGalerie avant */
+            ajaxEnterGalerie($(this));
+        });
+    });
+
+    $("#control-bar-album,#album-content").fadeIn();
+}
+
+/*
+ * enterGalerie(contents)
+ *
+ * Cette fonction permet de voir le contenue d'une galerie, elle va ajouter une barre de contrôle
+ * ainsi que charger le mur pour afficher toutes les items de contents.
+ *
+ */
+function enterGalerie(contents) {
+    $("#control-bar-album").fadeOut(function() {
+        addControlBar();
+        var albumTitle = contents.title;
+        $("h4").text(albumTitle);
+        $("h4").removeClass();
+        $("h4").addClass(
+            "col-xs-5 col-xs-offset-4 col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-3 centrer"
+        );
+    });
+    $("#control-bar-album,#album-content").fadeOut(function() {
+        $(".cell").remove();
+        wall.refresh();
+        wall.fitWidth();
+        loadWall(contents);
+    });
+    $("#control-bar-album,#album-content").fadeIn();
+}
+/*
+ *
+ *  loadWall(contents, callback)
+ *
+ *  Cette fonction permet d'initialiser le mur du plugin freewall en parcourant le tableau items[]
+ *  du parametres contents, la fonction va creer une case pour chaque objet dans ce tableau en
+ *  utilisant le lien nommé thumbnail de chaque items pour creer l'aperçu utilisé par la case.
+ */
+function loadWall(contents) {
+    var html = '';
+
+    for (var i = 0; i < contents.size; ++i) {
+        if (contents == contentGalerie)
+            html += "<a href='" + contents.items[i].link + "'>\n";
+        if(contents.items[i].id_galerie != undefined)
+            html += addNewCell(contents.items[i].title, contents.items[i].thumbnail, wallConfig.width,
+                wallConfig.height, contents.items[i].id_galerie);
+        else
+            html += addNewCell(contents.items[i].title, contents.items[i].thumbnail, wallConfig.width,
+                wallConfig.height);
+        if (contents == contentGalerie)
+            html += "</a>\n"
+    }
+
+    $("#album-content").append(html);
+
+    wall.reset({
+        selector: '.cell',
+        animate: true,
+        cellW: wallConfig.width,
+        cellH: wallConfig.height,
+        onResize: function() {
+            wall.refresh();
+        }
+    });
+    wall.fitWidth();
+    $(window).trigger("resize");
+}
+
+/*
+ * addNewCell(imgLink, width, height)
+ *
+ * Cette fonction retourne une variable qui contient le code html d'une case qui compose la galerie.
+ * On cree une case en donnant le lien vers le thumbnail/lien direct de l'image ainsi que les
+ * dimensions de la case qui va etre crée.
+ *
+ * Return: le code html d'une case
+ *
+ */
+function addNewCell(title, imgLink, width, height, id_galerie) {
+    var temp = "<div class='cell' ";
+    if (id_galerie != undefined)
+        temp += "data-id_galerie='" + id_galerie + "'";
+    temp += " style='width:" + width + "px; height:" + height +
+        "px;background-image: url(./" + imgLink + ")'>\n" +
+        "<div class='layer'>" + "<span class='desc'>" + title + "</span>" + "</div>\n</div>\n";
+    return temp;
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+
+    return "FAIL";
+}
+
+function prepareLoadingGif() {
+    $("#loading").hide();
+    $(document).ajaxStart(function() {
+        $("#loading").show();
+    });
+
+    $(document).ajaxStop(function() {
+        $("#loading").hide();
+    });
+
+}
+
+function ajaxEnterGalerie(cell){
+    $.ajax({
+        url: 'php/controller/get_galleries_and_images.php',
+        type: 'POST',
+        data: 'id_gal=' + cell.data("id_galerie") + '&op=contentGalerie',
+        dataType:'json',
+        success: function(data) {
+            contentGalerie = data;
+        },
+        complete: function(result, statut) {
+            enterGalerie(contentGalerie);
+        },
+        error: function(result, statut, erreur){
+            alert("Echec du chargement de la galerie, erreur: " + erreur);
+        }
+    });
+}
