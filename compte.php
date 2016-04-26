86b55ffea799a0689067635d9b13cb225e71bbdc
khorgoht <khorgoht@gmail.com>
2016-04-27 00:48:58 +0200

    ameliorer l'affichage du titre image

--- compte.php
+++ compte.php
@@ -23,7 +23,7 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 id="imageTitleM" class="modal-title">Titre</h4>
+                        <h3 id="imageTitleM" class="modal-title">Titre</h3>
                     </div>
                     <div class="modal-body">
                         <div>
